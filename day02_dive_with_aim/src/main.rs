d6ec0a22eb249148c79489772d775e3a5e4f52bc
Dirk Stolle <striezel-dev@web.de>
2024-09-08 21:33:14 +0200

    fix clippy warning in day 2, part 2

--- day02_dive_with_aim/src/main.rs
+++ day02_dive_with_aim/src/main.rs
@@ -15,35 +15,32 @@ fn main() {
     let mut depth = 0;
     let mut horizontal = 0;
     let mut aim = 0;
-    for l in lines
+    for line in lines.flatten()
     {
-        if let Ok(line) = l
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        if parts.len() != 2
         {
-            let parts: Vec<&str> = line.split_whitespace().collect();
-            if parts.len() != 2
-            {
-                eprintln!("Error: Invalid input '{}' encountered!", &line);
+            eprintln!("Error: Invalid input '{}' encountered!", &line);
+            return;
+        }
+        let number = parts[1].parse::<i32>();
+        if number.is_err()
+        {
+            eprintln!("Error: Could not parse input '{}' as number!", &parts[1]);
+            return;
+        }
+        let number = number.unwrap();
+        match parts[0]
+        {
+            "forward" => {
+                horizontal += number;
+                depth += aim * number;
+            },
+            "down" => aim += number,
+            "up" => aim -= number,
+            _ => {
+                eprintln!("Error: '{}' is not a valid movement direction!", &parts[0]);
                 return;
-            }
-            let number = parts[1].parse::<i32>();
-            if number.is_err()
-            {
-                eprintln!("Error: Could not parse input '{}' as number!", &parts[1]);
-                return;
-            }
-            let number = number.unwrap();
-            match parts[0]
-            {
-                "forward" => {
-                    horizontal += number;
-                    depth += aim * number;
-                },
-                "down" => aim += number,
-                "up" => aim -= number,
-                _ => {
-                    eprintln!("Error: '{}' is not a valid movement direction!", &parts[0]);
-                    return;
-                }
             }
         }
     }
